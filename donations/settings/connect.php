5acdd874b7276e04e9da8d2cde1cfe2336cb2542
BenTheExo <bhenderson904@gmail.com>
2012-03-04 12:45:59 -0500

    Fixed a glitch with the donations/settings/connect.php
    Signed-off-by: BenTheExo <bhenderson904@gmail.com>

--- donations/settings/connect.php
+++ donations/settings/connect.php
@@ -1,5 +1,5 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . '/csmp/config.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/csmp/config.php');
 /* End config */
 $link = @mysql_connect($CONFIG_rag_serv,$CONFIG_rag_user,$CONFIG_rag_pass) or die('Unable to establish a DB connection');
 mysql_set_charset('utf8');
